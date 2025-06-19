58ca66a80d72f41cd44edecef9aa60ee116b10e5
Aqazde <adil64489@gmail.com>
2025-06-20 00:24:11 +0500

    add: film controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,52 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import jakarta.validation.Valid;
+import java.time.LocalDate;
+import java.util.*;
+
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int nextId = 1;
+    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        if (film.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
+            log.warn("Дата релиза слишком старая: {}", film.getReleaseDate());
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года.");
+        }
+        film.setId(nextId++);
+        films.put(film.getId(), film);
+        log.info("Добавлен фильм: {}", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new ValidationException("Фильм с ID " + film.getId() + " не найден");
+        }
+        if (film.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
+            log.warn("Дата релиза старая: {}", film.getReleaseDate());
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+        films.put(film.getId(), film);
+        log.info("Обновлен фильм: {}", film);
+        return film;
+    }
+
+    @GetMapping
+    public Collection<Film> getAll() {
+        return films.values();
+    }
 }
